8e0b6d7afcebf16d6a98d0853ffc5a60154bc439
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2021-11-24 20:44:15 +0530

    done

--- components/Contact.js
+++ components/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
 </svg>
-                  </div>tejaskmail.com</div>
+                  </div>tejaskolwankar021mail.com</div>
                     <div className={styles.cInfoItem}>
                     <div className={styles.cIcon}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 text-red-600" viewBox="0 0 20 20" fill="currentColor">
