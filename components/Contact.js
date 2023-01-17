9f37733fc52cb1b32a99c9762bbbfe4e778a9af1
Tejas021 <tejaskolwankar021@gmail.com>
2023-01-17 19:27:50 +0530

    resume changed

--- components/Contact.js
+++ components/Contact.js
@@ -52,7 +52,7 @@ const Contact = () => {
   <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
 </svg>
                     </div>
-                    <p> 3/83 B Suragraj Anmol Society Uthkarshanagar Kuwarbav</p>
+                    <p> C 305 , Jayraj Nagar , 100ft road , Near Manav Mandir , Vasai West</p>
                   </div>
                 </div>
                 </div>
